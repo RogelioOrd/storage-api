eb1fa154976e8c99fb7d35151f1d53c84ac1cb7b
RogelioOrd <rogelio2610xd@gmail.com>
2021-07-19 19:12:08 -0700

    cambio route

--- routes/wikiinforoute.py
+++ routes/wikiinforoute.py
@@ -1,22 +1,23 @@
-from json import dumps as json_dumps
-from modules.auth import validate_token, auth_required
-import bottle
 from modules.bottles import BottleJson
 
 app = BottleJson()
+
+@app.get("/")
+@app.get("/<name>")
+@app.get("/<name>/<age:int>")
+def index(name="mariano", age=10):
+    payload = bottle.request.query_storage
+    print(name)
+    print(bottle.request.query_string)
+    print(payload.dict)
+    print(name, age)
+    raise bottle.HTTPError(501, 'Error')
 
 @app.post("/wiki-info/login")
 def store_record(*args, **kwargs):
     bottle.response.status = 501
     bottle.response.content_type = "application/json"
     return dict(code= 501, message = "Bad Request ")
-
-@app.get("/")
-def index():
-    payload = bottle.request.query_storage
-    print(bottle.request.query_string)
-    print(payload.dict)
-    raise bottle.HTTPError(501, 'Error')
 
 @app.get("/wiki-info/profile")
 def get_all_info(*args, **kwargs):
